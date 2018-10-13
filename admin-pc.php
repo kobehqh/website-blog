2a28588c9a9f5f32ead5fcda9e4936cfb799b516
hqh_service <1406152273@qq.com>
2018-10-13 22:16:03 +0800

    表单

--- admin-pc.php
+++ admin-pc.php
@@ -82,5 +82,78 @@
         </div>
     </div>
 </nav>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Open modal for @fat</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+...more buttons...
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="recipient-name" class="control-label">Recipient:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="form-group">
+                        <label for="message-text" class="control-label">Message:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+        </div>
+    </div>
+</div>
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>我的博客</th>
+        <th>我的博客</th>
+        <th>我的博客</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>电风扇</td>
+        <td>方式</td>
+        <td>方式</td>
+    </tr>
+    <tr>
+        <td>电风扇</td>
+        <td>方式</td>
+        <td>方式</td>
+    </tr>
+    <tr>
+        <td>电风扇</td>
+        <td>方式</td>
+        <td>方式</td>
+    </tr>
+    <tr>
+        <td>电风扇</td>
+        <td>方式</td>
+        <td>方式</td>
+    </tr>
+    </tbody>
+</table>
 </body>
+<script>
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    })
+</script>
 </html>
