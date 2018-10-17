bc47b5523bbc7eface86d16a442877655f1e58ac
hqh_service <1406152273@qq.com>
2018-10-17 23:39:23 +0800

    首页音频预加载

--- index.php
+++ index.php
@@ -46,6 +46,7 @@ $num = $to_result[0];
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/m.css" rel="stylesheet">
     <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/resLoader.js"></script>
     <script src="js/jquery.easyfader.min.js"></script>
     <script src="js/scrollReveal.js"></script>
     <script src="js/modernizr.js"></script>
@@ -217,6 +218,38 @@ $num = $to_result[0];
 <div class="more play" id="more"></div>
 <a href="#" class="cd-top">Top</a>
 </body>
+<script>
+    var loader = new resLoader({
+    resources : [
+                'http://p2.qhimg.com/t01ed1438874f940dc0.jpg',
+                'http://p9.qhimg.com/t01b4ff03b72c7dc6c7.jpg',
+                'http://p2.qhimg.com/t01dd90dfbec92074d0.jpg',
+                'http://p7.qhimg.com/t01cfec6d87cde457c5.jpg',
+                'http://p9.qhimg.com/t01943ced462da67833.jpg',
+                'http://p0.qhimg.com/t01943ced462da67833.jpg',
+                'http://p6.qhimg.com/t01aa15a7ba7ccb49a7.jpg',
+                'http://p8.qhimg.com/t010f1e8badf1134376.jpg',
+                'http://p8.qhimg.com/t01cf37ea915533a032.jpg',
+                'http://p3.qhimg.com/t0193d8a3963e1803e9.jpg',
+                'http://p3.qhimg.com/t01cd6a4d4b4bd4457b.jpg'
+            ],
+    onStart : function(total){
+    console.log('start:'+total);
+    },
+    onProgress : function(current, total){
+    console.log(current+'/'+total);
+    var percent = current/total*100;
+    $('.progressbar').css('width', percent+'%');
+    $('.progresstext .current').text(current);
+    $('.progresstext .total').text(total);
+    },
+    onComplete : function(total){
+    alert('加载完毕:'+total+'个资源');
+    }
+    });
+
+    loader.start();
+</script>
 <script>
     window.onload=function(){
         document.getElementById('audio').load();
